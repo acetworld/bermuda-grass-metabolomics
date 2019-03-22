2296acba7aecd42d6834effaa421cb5cebfa0998
Denis Willett(User) <denis@dhcp-ccc-6141.eduroam.cornell.edu>
2019-03-22 13:27:38 -0400

    updated analysis scripts

---
+++ analysis/nematode_analysis.R
@@ -0,0 +1,380 @@
+#########################################
+#                                       #
+#       Bermuda Grass Metabolomics      #
+#                                       #
+#########################################
+
+
+# Analysis of Nematode Count and Root Biomass Data
+
+# ACET Lab
+# Denis Willett
+# March 2019
+
+source('./analysis/packages.R')
+
+
+# Load in data -------------------------------------
+
+# Read in Counts of Sting nematode and biomass of roots
+nematode_counts <- read_csv('./data/nematode_counts.csv', skip = 5)
+
+# Nematode Counts Analysis-------------------------------------
+
+# Visualize nematode counts
+dodge_width = 0.48
+jitter_width = 0.06
+fig1_nematode_counts <- ggplot(nematode_counts, aes(x = line, y = nematode_count, color = treatment)) + 
+    geom_point(position = position_jitterdodge(jitter.width = jitter_width,
+                                               dodge.width = dodge_width),
+               alpha = 0.72) + 
+    stat_summary(fun.data = 'mean_cl_boot', geom = 'errorbar',
+                 position = position_dodge(dodge_width), 
+                 width = dodge_width/2) + 
+    stat_summary(fun.data = 'mean_cl_boot', geom = 'point',
+                 position = position_dodge(dodge_width)) + 
+    scale_color_grey(labels = c('Inoculated', 'Uninoculated')) +
+    theme_bw() + 
+    theme(legend.title = element_blank(),
+          legend.position = c(0.75, 0.124),
+          legend.direction = 'horizontal',
+          legend.background = element_rect(color = 'grey30')) + 
+    labs(x = 'Line', y = 'Sting Nematode Count')
+
+ggsave(plot = fig1_nematode_counts, filename = './figures/fig1-nematode-counts.pdf',
+       width = 8, height = 5)
+
+
+# Model Nematode Counts
+
+# baseline model for comparison
+baseline_count_mod = lm(nematode_count ~ 1,
+                        data = nematode_counts)
+
+# model effects of line, treatment, and their interaction on nematode counts
+nematode_count_mod <- lm(nematode_count ~ line * treatment, 
+                         data = nematode_counts)
+
+# Model summary and diagnosis
+summary(nematode_count_mod)
+Anova(nematode_count_mod, type = 'III')
+AIC(nematode_count_mod)
+BIC(nematode_count_mod)
+lrtest(nematode_count_mod, baseline_count_mod)
+qqPlot(nematode_count_mod$residuals)
+plot(nematode_count_mod, which = 1:6)
+
+# Post-hoc analysis of nematode counts
+nematode_count_emm <- emmeans(nematode_count_mod, ~ line * treatment)
+nematode_count_emm %>% contrast() %>% cld()
+
+contrast(nematode_count_emm, by = 'treatment', method = 'pairwise')
+
+# Root Biomass Analysis --------------------------------
+
+
+# Visualize Nematode Weights
+dodge_width = 0.48
+jitter_width = 0.06
+# note: outliers removed - see modeling analysis below
+fig2_root_weights <- ggplot(nematode_counts[-c(31,30),], aes(x = line, y = weight, color = treatment)) +
+    geom_point(position = position_jitterdodge(jitter.width = jitter_width,
+                                               dodge.width = dodge_width),
+               alpha = 0.72) +
+    stat_summary(fun.data = 'mean_cl_boot', geom = 'errorbar',
+                 position = position_dodge(dodge_width),
+                 width = dodge_width/2) + 
+    stat_summary(fun.data = 'mean_cl_boot', geom = 'point',
+                 position = position_dodge(dodge_width)) + 
+    scale_color_grey(labels = c('Inoculated', 'Uninoculated')) + 
+    theme_bw() +
+    theme(legend.title = element_blank(),
+          legend.position = c(0.9, 0.124),
+          legend.background = element_rect(color = 'grey30')) + 
+    labs(x = 'Line', y = 'Root Biomass (g)')
+
+ggsave(plot = fig2_root_weights, filename = './figures/fig2-root-weights.pdf',
+       width = 8, height = 5)
+
+# Model root biomass effects 
+
+# baseline mass model for comparison
+baseline_mass_mod <- lm(weight ~ 1,
+                        data = nematode_counts)
+
+# Model effect of line, treatment, and nematode counts on root biomass
+mass_mod <- lm(weight ~ line * treatment + nematode_count, 
+               data = nematode_counts)
+
+# Model summary and diagnostics
+summary(mass_mod)
+Anova(mass_mod, type = 'III')
+AIC(mass_mod)
+BIC(mass_mod)
+lrtest(mass_mod, baseline_mass_mod)
+qqPlot(mass_mod$residuals)
+plot(mass_mod, which = 1:6)
+
+# it looks like we have two pretty large outliers. Let's double check that.
+outlierTest(mass_mod)
+
+
+# Test removing observation 31
+mass_mod <- lm(weight ~ line * treatment + nematode_count, 
+               data = nematode_counts[-31,])
+
+summary(mass_mod)
+Anova(mass_mod, type = 'III')
+AIC(mass_mod)
+BIC(mass_mod)
+qqPlot(mass_mod$residuals)
+plot(mass_mod, which = 1:6)
+outlierTest(mass_mod)
+
+# test removing both observations
+counts_no_outlier <- nematode_counts %>% slice(-c(30,31))
+
+# new baseline model
+baseline_mass_no_outlier <- lm(weight ~ 1, data = counts_no_outlier)
+
+# Model without outliers
+mass_mod_no_outlier <- lm(weight ~ line + treatment, 
+                          data = counts_no_outlier)
+
+# Model summary and diagnostics
+summary(mass_mod_no_outlier)
+Anova(mass_mod_no_outlier, type = 'III')
+AIC(mass_mod_no_outlier)
+BIC(mass_mod_no_outlier)
+lrtest(mass_mod_no_outlier, baseline_mass_no_outlier)
+qqPlot(mass_mod_no_outlier$residuals)
+plot(mass_mod_no_outlier, which = 1:6)
+outlierTest(mass_mod_no_outlier)
+
+# post-hoc
+mass_emm <- emmeans(mass_mod_no_outlier, ~ line + treatment)
+mass_emm %>% contrast() %>% cld()
+
+contrast(mass_emm, by = 'line', method = 'pairwise')
+contrast(mass_emm, by = 'treatment', method = 'pairwise')
+
+
+# Root biomass loss analysis -------------------------------
+
+# We'll use bootstrapping to get at estimates of loss 
+# since we do not have exact values
+
+# number of bootstrap reps
+bootstrap_reps = 1000
+
+# select only those plants with nematodes
+inoculated_plants <- filter(counts_no_outlier, treatment == 'inoc')
+
+# select only those plants without nematodes for baseline
+uninoculated_plants <- filter(counts_no_outlier, treatment == 'uninoc')
+
+# holder for bootstrapped data:
+bootstrapped_loss <- tibble()
+bootstrapped_loss_model <- tibble()
+
+# function for fitting model to results
+loss_model <- function(df) {
+    lm(loss ~ nematode_count, data = df)
+}
+
+# Set Seed for reproducibility
+set.seed(72)
+
+# Bootstrap loss by sampling with replacement uninoculated plants,
+# calculating baseline mean, then subtracting that mean from observed 
+# plants inoculated with nematodes
+for (i in seq(1:bootstrap_reps)) {
+    
+    # sample with replacement and calculate means
+    uninoculated_means <- uninoculated_plants %>%
+        group_by(line) %>% 
+        sample_frac(replace = TRUE) %>%
+        summarize(uninoc_mn = mean(weight))
+    
+    # match means by line and calculate loss
+    loss_of_biomass <- left_join(inoculated_plants, uninoculated_means) %>%
+        mutate(loss = uninoc_mn - weight,
+               bootstrap_rep = i)
+    
+    # record samples for analysis
+    bootstrapped_loss <- bind_rows(bootstrapped_loss, loss_of_biomass)
+    
+    # model relationship between nematode counts and weights
+    biomass_loss_model <- loss_of_biomass %>%
+        mutate(line = as.factor(line)) %>%
+        select(line, nematode_count, loss) %>% 
+        group_by(line) %>% 
+        nest() %>%
+        mutate(model = purrr::map(data, loss_model),
+               model_coefs = purrr::map(model, coef),
+        ) %>%
+        select(line, model_coefs) %>%
+        unnest() %>%
+        mutate(bootstrap_rep = i,
+               coefficient = rep(c('intercept', 'nematode_count'),3))
+    
+    bootstrapped_loss_model <- bind_rows(bootstrapped_loss_model, biomass_loss_model)
+    
+}
+
+bootstrapped_loss_plot <- bootstrapped_loss %>%
+    select(line, bootstrap_rep, loss) %>%
+    group_by(line, bootstrap_rep) %>%
+    summarize(median = median(loss)) %>%
+    group_by(line) %>%
+    summarize(med_med = median(median),
+              low = quantile(median, 0.025),
+              high = quantile(median, 0.975))
+
+
+fig3_root_loss <- ggplot(bootstrapped_loss_plot, 
+                         aes(x = line, y = med_med, color = line)) + 
+    geom_point() + 
+    geom_errorbar(aes(x = line, ymin = low, ymax = high)) + 
+    theme_bw() +
+    scale_color_grey() + 
+    labs(x = 'Line', y = 'Biomass Loss (g)') + 
+    theme(legend.position = 'none')
+
+ggsave(plot = fig3_root_loss, filename = './figures/fig3-root-loss.pdf',
+       width = 8, height = 5)
+
+bootstrap_test <- bootstrapped_loss %>% 
+    select(line, bootstrap_rep, loss) %>% 
+    group_by(line, bootstrap_rep) %>% 
+    summarize(mn = median(loss)) %>% 
+    spread(line, mn) %>%
+    mutate(AB39_AB03 = AB39 > AB03,
+           AB39_AB33 = AB39 > AB33,
+           AB33_AB03 = AB33 > AB03) %>%
+    select(AB39_AB03, AB39_AB33, AB33_AB03) %>%
+    gather(key = 'test', value = 'comparison_result') %>%
+    group_by(test) %>%
+    summarize(val = mean(comparison_result)) %>%
+    mutate(bonferroni_correction = val * 3)
+
+
+---------------------------------------------------
+    
+    
+    
+    
+    mean(test$test)
+
+ggplot(bootstrapped_loss, aes(x = line, y = loss)) +
+    geom_violin() +
+    theme_bw()
+
+ggplot(bootstrapped_loss, aes(x = nematode_count, y = loss, 
+                              color = line, shape = line)) +
+    geom_point() + 
+    stat_smooth(method = 'lm', level = 0.99) + 
+    scale_color_grey() + 
+    theme_bw() +
+    geom_abline(slope = -0.000160, intercept = 0.213)
+
+ggplot(filter(bootstrapped_loss_model, coefficent == 'intercept'),
+       aes(x = model_coefs, fill = line)) + 
+    stat_density() 
+
+ggplot(filter(bootstrapped_loss_model, coefficent == 'nematode_count'),
+       aes(x = model_coefs, fill = line)) +
+    stat_density()
+
+test <- loss_of_biomass %>% mutate(line = as.factor(line)) %>%
+    select(line, nematode_count, loss) %>%
+    group_by(line) %>%
+    nest() %>% 
+    mutate(model = purrr::map(data, loss_model),
+           model_coefs = purrr::map(model, coef))
+
+test %>% select(line, model_coefs) %>% unnest()
+
+ggplot(nematode_counts, 
+       aes(x = nematode_count, y = weight, 
+           color = treatment, shape = line)) + 
+    geom_point() + 
+    scale_color_grey() + 
+    theme_bw()
+
+ggplot(filter(nematode_counts, treatment == 'inoc'), 
+       aes(x = nematode_count, y = weight, color = line)) + 
+    geom_point() + 
+    scale_color_grey() + 
+    #    stat_smooth(method = 'lm') + 
+    theme_bw()
+
+
+filter(bootstrapped_loss_model, coefficent == 'intercept') %>%
+    group_by(line) %>%
+    summarize(low = quantile(model_coefs, 0.05),
+              high = quantile(model_coefs, 0.95))
+
+test <- filter(bootstrapped_loss_model, coefficent == 'intercept') %>%
+    spread(line, model_coefs) %>%
+    mutate(yes = AB33 > AB39)
+
+mean(test$yes)
+
+
+baseline <- lm(weight ~ line, data = filter(nematode_counts, treatment == 'uninoc'))
+summary(baseline)
+Anova(baseline)
+plot(baseline)        
+
+predict(baseline, newdata = filter(nematode_counts, treatment == 'inoc'))
+
+# Bootstrapping estimates of loss -------------------------
+bootstrap_reps = 1000
+
+# select only those plants with nematodes
+inoculated_plants <- filter(nematode_counts[-c(30, 31),], treatment == 'inoc')
+
+# select only those plants without nematodes for baseline
+uninoculated_plants <- filter(nematode_counts[-c(30, 31),], treatment == 'uninoc')
+
+# holder for bootstrapped data:
+bootstrapped_loss <- tibble()
+
+# Bootstrap loss by sampling with replacement uninoculated plants,
+# calculating baseline mean, then subtracting that mean from observed 
+# plants inoculated with nematodes
+for (i in seq(1:bootstrap_reps)) {
+    
+    # sample with replacement and calculate means
+    uninoculated_means <- uninoculated_plants %>%
+        group_by(line) %>% 
+        sample_frac(replace = TRUE) %>%
+        summarize(uninoc_mn = mean(weight))
+    
+    # match means by line and calculate loss
+    loss_of_biomass <- left_join(inoculated_plants, uninoculated_means) %>%
+        mutate(loss = uninoc_mn - weight,
+               bootstrap_rep = i)
+    
+    # record samples for analysis
+    bootstrapped_loss <- bind_rows(bootstrapped_loss, loss_of_biomass)
+    
+}
+
+ggplot(bootstrapped_loss, aes(x = nematode_count, y = loss, 
+                              color = line, shape = line)) +
+    geom_point() + 
+    stat_smooth(method = 'lm') + 
+    scale_color_grey() + 
+    theme_bw()
+
+weight_mod <- lm(weight ~ line + treatment + nematode_count, data = nematode_counts[-c(30, 31),])
+summary(weight_mod)
+Anova(weight_mod, type = 'III')
+plot(weight_mod, which = 1:6)
+qqPlot(weight_mod$residuals)
+outlierTest(weight_mod)
+
+test_diff <- emmeans(weight_mod, ~ line + treatment)
+contrast(test_diff, method = 'pairwise') %>% cld()
---
+++ analysis/preprocess_metabolite_data.R
@@ -0,0 +1,208 @@
+source('./analysis/load_metabolite_data.R')
+
+# Combine common compounds ------------------------------
+
+# We want to combine results for compunds that have more than 
+# one retention time.  To do this, we'll collapse all labeled compounds
+metabolite_combined <- metabolite_data %>% 
+    mutate(compounds = str_replace(Samples, '_R[12345678]', '')) %>% 
+    select(-Samples) %>%
+    group_by(compounds, ion_type) %>% 
+    summarize_all(funs(sum)) %>%
+    ungroup()
+
+
+# Remove contaminants and adjust for internal standards ------------------------------
+
+# We'll remove phthalates
+# Also remove Dinoterb (applied herbicide) and ibuprofen
+
+no_contaminants <- metabolite_combined %>% 
+    filter(!(compounds %in% c('Dinoterb (herbicide)',
+                              'Ibuprofen')),
+           !(grepl('phthal', .$compounds, ignore.case = TRUE)))
+
+# Remove internal standards referenced in procedures:
+standard_blend <- tibble(
+    compound = c('Tryptophan-2,3,3-D3', 
+                 'L-Leucine-D10',
+                 'Creatine-D3',
+                 'CREATINE',
+                 'Caffeine-D3',
+                 'Caffeine 13C3',
+                 'Salicylic Acid-D4',
+                 'Succinic-2,2,3,3-D4 Acid',
+                 'CITRIC ACID-13C6'),
+    concentration_ug_ml = c(40, rep(4, 8))
+)
+
+clean_metabolites <- no_contaminants %>% 
+    filter(!(compounds %in% standard_blend$compound))
+
+
+# Imputation -----------------------------------
+
+# We have missing data in our files 
+clean_metabolites %>% gather(sample, value, -compounds, -ion_type) %>% 
+    group_by(ion_type, sample) %>% 
+    summarize(num_nas = sum(is.na(value)),
+              perc_nas = num_nas/n()) %>% 
+    arrange(perc_nas) %>%
+    data.frame()
+
+# We'll impute (Fill-in) the missing data using a K-nearest neighbors approach
+
+metabolite_imputation <- kNN(clean_metabolites[,3:43], imp_var = FALSE, 
+                   useImputedDist = FALSE)
+
+
+# Normalize ----------------------------------
+
+# For normalization we'll use variance stabilizing normalization
+# which tends to have high performance (Li et al 2016) and has the nice
+# benefit of tending to remove heteroscedasticity
+es_nema <- ExpressionSet(as.matrix(metabolite_imputation))
+
+vsn_model <- vsn2(es_nema)
+meanSdPlot(vsn_model)
+
+# Processed nematode grass metabolite data
+processed_metabolites <- cbind(clean_metabolites[,1:2],
+                        vsn_model@hx)
+
+# Refined compound names for display
+compound_names <- processed_metabolites %>%
+    select(compounds) %>%
+    mutate(numeric_names = as.numeric(compounds)) %>%
+    filter(is.na(numeric_names)) %>%
+    select(-numeric_names) %>% 
+    mutate(revised_names = c('(R)-Malate',
+                             'ACC',
+                             '2,3-Dihydroxybenzoate',
+                             '2,5-Dihydroxybenzoate',
+                             '2,4-Dihydroxyacetophenone',
+                             '3-Hydroxyphenyl Lactate',
+                             '3-Hydroxyphenyl Acetate',
+                             '3-Methyl Adipic Acid',
+                             '3-Tert-Butyl Adipic Acid',
+                             '3,4-Dihydroxybenzoate',
+                             '3,4-Dihydroxyphenyl Acetate',
+                             '4-Aminobutanoate',
+                             '4-Guanidinobutanoate',
+                             '4-Hydroxy-L-Phenylglycine',
+                             'Oxoproline',
+                             '5-Deoxyadenosine',
+                             '6-Hydroxycaproic Acid',
+                             '6-Phosphogluconic Acid',
+                             '6C Sugar Alcohol',
+                             'a-Ketoglutaric Acid',
+                             'Adenine',
+                             'ADP',
+                             'Alanine/Sarcosine',
+                             'Aldo/Keto-Hexose',
+                             'Aldopentose',
+                             'Allopurinol',
+                             'a-Aminoadipate',
+                             'a-Ketoglutaric Acid',
+                             'Anthranilate',
+                             'Arachidonic Acid',
+                             'Arachidonic Acid',
+                             'Asparagine',
+                             'Aspartate',
+                             'Aspartate',
+                             'Benzoate',
+                             'Betaine',
+                             'C12-Disaccharide',
+                             'C5 Sugar Alcohol',
+                             'Hexose',
+                             'Caffeate',
+                             'Camphor',
+                             'Citrate',
+                             'Citrate',
+                             'Citrulline',
+                             'Cytosine',
+                             'D-(+)-Raffinose',
+                             'D-Glucaronic Acid',
+                             'D-Saccharic Acid',
+                             'Diethanolamine',
+                             'Dipropylene Glycol',
+                             'C6 Disaccharide',
+                             'Erucamide',
+                             'Erythritol',
+                             'Eugenol/Isoeugenol',
+                             'Ferulate',
+                             'Fumaric Acid',
+                             'Gluconic Acid',
+                             'Glucosamine',
+                             'Glucose/Fructose',
+                             'Glucose/Fructose',
+                             'Glutathione Disulfide',
+                             'Glyceraldehyde',
+                             'Glyceraldehyde/Lactate',
+                             'Glyceric Acid',
+                             'Glycerol',
+                             'Glycolate',
+                             'Guanine',
+                             'Guanosine',
+                             'Hexose Alcohol',
+                             'Hexose-6-Phosphate',
+                             'Hexose-Disaccharide',
+                             'Homovanillate',
+                             'Inosine',
+                             'Isocitric Acid',
+                             'Isocytosine',
+                             'L-(+)-Lactic Acid', 
+                             'L-2-Phosphoglyceric Acid',
+                             'L-Carnitine',
+                             'L-Cysteic Acid',                                              
+                             'L-Glutamic Acid',                                             
+                             'L-Glutamine',                                                
+                             'L-Isoleucine',                                            
+                             'L-Methionine',                                               
+                             'L-Proline',                                                   
+                             'L-Tyrosine',                                                  
+                             'Leucine',
+                             'Linoleic Acid',
+                             '2-6-Diaminoheptanedioate',
+                             'LysoPC',
+                             'LysoPE',
+                             'Malate',
+                             'Malanate',
+                             'Methyl-Beta-D-Galactoside',
+                             'N-Acetylneuraminate',
+                             'N-Alpha-Acetyl-L-Lysine',
+                             'N-Butylbenzenesulfonamide',
+                             'N-Methyl-D-Aspartic Acid',
+                             'Nonanoate',
+                             'Orthophosphate',
+                             'Palmitoleic Acid',
+                             'Pantothenic Acid',
+                             'PEG N5',
+                             'Phenylethylamine',
+                             'Phenylalanine',
+                             'Phenylalanine-HCOOH',
+                             'Phosphocholine',
+                             'Picolinic Acid',
+                             'Pipecolate/L-Pipecolic Acid',
+                             'Pyridoxal',
+                             'Pyroglutamic Acid',
+                             'Quinate',
+                             'Quinate',
+                             'Spermidine',
+                             'Stachyose',
+                             'Stachyose',
+                             'Succinate',
+                             'Succinate',
+                             'Sulcatol',
+                             'Tartaric Acid',
+                             'Theophylline',
+                             'Threonine/Homoserine',
+                             'Trans-Cinnamate',
+                             'Triethyl-Phosphate',
+                             'Trigonelline',
+                             'Tryptophan',
+                             'Tryptophan-NH3',
+                             'Urate',
+                             'Uridine',
+                             'Urocanate'))
+
